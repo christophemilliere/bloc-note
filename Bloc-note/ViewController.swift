c6383a73ff96ac221c7d6329eba4829ca77a6d5e
christophe <christophemilliere93@gmail.com>
2017-10-15 19:01:54 +0200

    Initial Commit

---
+++ Bloc-note/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bloc-note
+//
+//  Created by christophe milliere on 15/10/2017.
+//  Copyright © 2017 christophe milliere. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
